e1c41bee76a76569f340be018046ab81e5767875
Retreat-hunter <hunterkorsen@gmail.com>
2023-10-19 15:31:24 -0700

    done

---
+++ Script.js
@@ -0,0 +1,15 @@
+
+    var word = "green bottles";
+    var count = 10;
+
+    while (count > 0) {
+        console.log (count + " " + word + " hanging on the wall");
+        console.log (count + " " + word + " hanging on the wall");
+        console.log ("and if one green bottle should accidentally fall")
+        count = count -1;
+
+        if (count <= 0) {
+            console.log ("There'll be no more" + word + "hanging on the wall");
+        }
+    }
+    
